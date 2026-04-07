3a5e779309fe973b08d5ebaac0f3db939c3c7bff
TysonYu <784250886@qq.com>
2026-04-07 02:32:50 +0000

    Deploying to gh-pages from @ TysonYu/TysonYu.github.io@580a7bf5bc4ef60530010af3793e20ce192dd238 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -383,7 +383,12 @@ ninja.data = [{
           section: "News",},{id: "news-we-release-our-survey-on-agent-as-a-judge",
           title: 'We release our survey on Agent-as-a-Judge.',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-",
+          title: '',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_3/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
